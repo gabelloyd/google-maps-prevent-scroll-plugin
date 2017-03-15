fa94bba8884451e01c6908f77f8155c8295391ac
gabelloyd <gabe@longtailcreative.com>
2017-03-15 11:55:50 -0400

    Initial Commit. Update all file names.

---
+++ classes/class-starter-plugin-admin.php
@@ -0,0 +1,226 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * gMap_PreventScroll_Admin Class
+ *
+ * @class gMap_PreventScroll_Admin
+ * @version	1.0.0
+ * @since 1.0.0
+ * @package	gMap_PreventScroll
+ * @author Jeffikus
+ */
+final class gMap_PreventScroll_Admin {
+	/**
+	 * gMap_PreventScroll_Admin The single instance of gMap_PreventScroll_Admin.
+	 * @var 	object
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	private static $_instance = null;
+
+	/**
+	 * The string containing the dynamically generated hook token.
+	 * @var     string
+	 * @access  private
+	 * @since   1.0.0
+	 */
+	private $_hook;
+
+	/**
+	 * Constructor function.
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public function __construct () {
+		// Register the settings with WordPress.
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
+
+		// Register the settings screen within WordPress.
+		add_action( 'admin_menu', array( $this, 'register_settings_screen' ) );
+	} // End __construct()
+
+	/**
+	 * Main gMap_PreventScroll_Admin Instance
+	 *
+	 * Ensures only one instance of gMap_PreventScroll_Admin is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @static
+	 * @return Main gMap_PreventScroll_Admin instance
+	 */
+	public static function instance () {
+		if ( is_null( self::$_instance ) )
+			self::$_instance = new self();
+		return self::$_instance;
+	} // End instance()
+
+	/**
+	 * Register the admin screen.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function register_settings_screen () {
+		$this->_hook = add_submenu_page( 'options-general.php', __( 'Google Maps Prevent Scroll Settings', 'google-maps-prevent-scroll-plugin' ), __( 'Google Maps Prevent Scroll', 'google-maps-prevent-scroll-plugin' ), 'manage_options', 'google-maps-prevent-scroll-plugin', array( $this, 'settings_screen' ) );
+	} // End register_settings_screen()
+
+	/**
+	 * Output the markup for the settings screen.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function settings_screen () {
+		global $title;
+		$sections = gMap_PreventScroll()->settings->get_settings_sections();
+		$tab = $this->_get_current_tab( $sections );
+		?>
+		<div class="wrap google-maps-prevent-scroll-plugin-wrap">
+			<?php
+				echo $this->get_admin_header_html( $sections, $title );
+			?>
+			<form action="options.php" method="post">
+				<?php
+					settings_fields( 'google-maps-prevent-scroll-plugin-settings-' . $tab );
+					do_settings_sections( 'google-maps-prevent-scroll-plugin-' . $tab );
+					submit_button( __( 'Save Changes', 'google-maps-prevent-scroll-plugin' ) );
+				?>
+			</form>
+		</div><!--/.wrap-->
+		<?php
+	} // End settings_screen()
+
+	/**
+	 * Register the settings within the Settings API.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function register_settings () {
+		$sections = gMap_PreventScroll()->settings->get_settings_sections();
+		if ( 0 < count( $sections ) ) {
+			foreach ( $sections as $k => $v ) {
+				register_setting( 'google-maps-prevent-scroll-plugin-settings-' . sanitize_title_with_dashes( $k ), 'google-maps-prevent-scroll-plugin-' . $k, array( $this, 'validate_settings' ) );
+				add_settings_section( sanitize_title_with_dashes( $k ), $v, array( $this, 'render_settings' ), 'google-maps-prevent-scroll-plugin-' . $k, $k, $k );
+			}
+		}
+	} // End register_settings()
+
+	/**
+	 * Render the settings.
+	 * @access  public
+	 * @param  array $args arguments.
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function render_settings ( $args ) {
+		$token = $args['id'];
+		$fields = gMap_PreventScroll()->settings->get_settings_fields( $token );
+
+		if ( 0 < count( $fields ) ) {
+			foreach ( $fields as $k => $v ) {
+				$args 		= $v;
+				$args['id'] = $k;
+
+				add_settings_field( $k, $v['name'], array( gMap_PreventScroll()->settings, 'render_field' ), 'google-maps-prevent-scroll-plugin-' . $token , $v['section'], $args );
+			}
+		}
+	} // End render_settings()
+
+	/**
+	 * Validate the settings.
+	 * @access  public
+	 * @since   1.0.0
+	 * @param   array $input Inputted data.
+	 * @return  array        Validated data.
+	 */
+	public function validate_settings ( $input ) {
+		$sections = gMap_PreventScroll()->settings->get_settings_sections();
+		$tab = $this->_get_current_tab( $sections );
+		return gMap_PreventScroll()->settings->validate_settings( $input, $tab );
+	} // End validate_settings()
+
+	/**
+	 * Return marked up HTML for the header tag on the settings screen.
+	 * @access  public
+	 * @since   1.0.0
+	 * @param   array  $sections Sections to scan through.
+	 * @param   string $title    Title to use, if only one section is present.
+	 * @return  string 			 The current tab key.
+	 */
+	public function get_admin_header_html ( $sections, $title ) {
+		$defaults = array(
+							'tag' => 'h2',
+							'atts' => array( 'class' => 'google-maps-prevent-scroll-plugin-wrapper' ),
+							'content' => $title
+						);
+
+		$args = $this->_get_admin_header_data( $sections, $title );
+
+		$args = wp_parse_args( $args, $defaults );
+
+		$atts = '';
+		if ( 0 < count ( $args['atts'] ) ) {
+			foreach ( $args['atts'] as $k => $v ) {
+				$atts .= ' ' . esc_attr( $k ) . '="' . esc_attr( $v ) . '"';
+			}
+		}
+
+		$response = '<' . esc_attr( $args['tag'] ) . $atts . '>' . $args['content'] . '</' . esc_attr( $args['tag'] ) . '>' . "\n";
+
+		return $response;
+	} // End get_admin_header_html()
+
+	/**
+	 * Return the current tab key.
+	 * @access  private
+	 * @since   1.0.0
+	 * @param   array  $sections Sections to scan through for a section key.
+	 * @return  string 			 The current tab key.
+	 */
+	private function _get_current_tab ( $sections = array() ) {
+		if ( isset ( $_GET['tab'] ) ) {
+			$response = sanitize_title_with_dashes( $_GET['tab'] );
+		} else {
+			if ( is_array( $sections ) && ! empty( $sections ) ) {
+				list( $first_section ) = array_keys( $sections );
+				$response = $first_section;
+			} else {
+				$response = '';
+			}
+		}
+
+		return $response;
+	} // End _get_current_tab()
+
+	/**
+	 * Return an array of data, used to construct the header tag.
+	 * @access  private
+	 * @since   1.0.0
+	 * @param   array  $sections Sections to scan through.
+	 * @param   string $title    Title to use, if only one section is present.
+	 * @return  array 			 An array of data with which to mark up the header HTML.
+	 */
+	private function _get_admin_header_data ( $sections, $title ) {
+		$response = array( 'tag' => 'h2', 'atts' => array( 'class' => 'google-maps-prevent-scroll-plugin-wrapper' ), 'content' => $title );
+
+		if ( is_array( $sections ) && 1 < count( $sections ) ) {
+			$response['content'] = '';
+			$response['atts']['class'] = 'nav-tab-wrapper';
+
+			$tab = $this->_get_current_tab( $sections );
+
+			foreach ( $sections as $key => $value ) {
+				$class = 'nav-tab';
+				if ( $tab == $key ) {
+					$class .= ' nav-tab-active';
+				}
+
+				$response['content'] .= '<a href="' . admin_url( 'options-general.php?page=google-maps-prevent-scroll-plugin&tab=' . sanitize_title_with_dashes( $key ) ) . '" class="' . esc_attr( $class ) . '">' . esc_html( $value ) . '</a>';
+			}
+		}
+
+		return (array)apply_filters( 'google-maps-prevent-scroll-plugin-get-admin-header-data', $response );
+	} // End _get_admin_header_data()
+} // End Class
---
+++ starter-plugin.php
@@ -0,0 +1,205 @@
+<?php
+/**
+ * Plugin Name: Google Maps Prevent Scroll
+ * Plugin URI: https://longtailcreative.com/google-maps-prevent-scroll-plugin/
+ * Description: Wordpress Plugin to prevent the scroll of Google Maps windows. Sourced from https://github.com/diazemiliano/googlemaps-scrollprevent
+ * Version: 1.0.0
+ * Author: Gabe Lloyd
+ * Author URI: https://longtailcreative.com
+ * Requires at least: 4.0.0
+ * Tested up to: 4.0.0
+ *
+ * Text Domain: google-maps-prevent-scroll-plugin
+ * Domain Path: /languages/
+ *
+ * @package gMap_PreventScroll
+ * @category Core
+ * @author Gabe Lloyd
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Returns the main instance of gMap_PreventScroll to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object gMap_PreventScroll
+ */
+function gMap_PreventScroll() {
+	return gMap_PreventScroll::instance();
+} // End gMap_PreventScroll()
+
+add_action( 'plugins_loaded', 'gMap_PreventScroll' );
+
+/**
+ * Main gMap_PreventScroll Class
+ *
+ * @class gMap_PreventScroll
+ * @version	1.0.0
+ * @since 1.0.0
+ * @package	gMap_PreventScroll
+ * @author Gabe Lloyd
+ */
+final class gMap_PreventScroll {
+	/**
+	 * gMap_PreventScroll The single instance of gMap_PreventScroll.
+	 * @var 	object
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	private static $_instance = null;
+
+	/**
+	 * The token.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $token;
+
+	/**
+	 * The version number.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $version;
+
+	/**
+	 * The plugin directory URL.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $plugin_url;
+
+	/**
+	 * The plugin directory path.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $plugin_path;
+
+	// Admin - Start
+	/**
+	 * The admin object.
+	 * @var     object
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $admin;
+
+	/**
+	 * The settings object.
+	 * @var     object
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $settings;
+	// Admin - End
+
+	// Post Types - Start
+	/**
+	 * The post types we're registering.
+	 * @var     array
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $post_types = array();
+	// Post Types - End
+	/**
+	 * Constructor function.
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public function __construct () {
+		$this->token 			= 'google-maps-prevent-scroll-plugin';
+		$this->plugin_url 		= plugin_dir_url( __FILE__ );
+		$this->plugin_path 		= plugin_dir_path( __FILE__ );
+		$this->version 			= '1.0.0';
+
+		// Admin - Start
+		require_once( 'classes/class-google-maps-prevent-scroll-plugin-settings.php' );
+			$this->settings = gMap_PreventScroll_Settings::instance();
+
+		if ( is_admin() ) {
+			require_once( 'classes/class-google-maps-prevent-scroll-plugin-admin.php' );
+			$this->admin = gMap_PreventScroll_Admin::instance();
+		}
+		// Admin - End
+
+		// Post Types - Start
+		require_once( 'classes/class-google-maps-prevent-scroll-plugin-post-type.php' );
+		require_once( 'classes/class-google-maps-prevent-scroll-plugin-taxonomy.php' );
+
+		// Register an example post type. To register other post types, duplicate this line.
+		$this->post_types['thing'] = new gMap_PreventScroll_Post_Type( 'thing', __( 'Thing', 'google-maps-prevent-scroll-plugin' ), __( 'Things', 'google-maps-prevent-scroll-plugin' ), array( 'menu_icon' => 'dashicons-carrot' ) );
+		// Post Types - End
+		register_activation_hook( __FILE__, array( $this, 'install' ) );
+
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+	} // End __construct()
+
+	/**
+	 * Main gMap_PreventScroll Instance
+	 *
+	 * Ensures only one instance of gMap_PreventScroll is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @static
+	 * @see gMap_PreventScroll()
+	 * @return Main gMap_PreventScroll instance
+	 */
+	public static function instance () {
+		if ( is_null( self::$_instance ) )
+			self::$_instance = new self();
+		return self::$_instance;
+	} // End instance()
+
+	/**
+	 * Load the localisation file.
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain( 'google-maps-prevent-scroll-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	} // End load_plugin_textdomain()
+
+	/**
+	 * Cloning is forbidden.
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function __clone () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), '1.0.0' );
+	} // End __clone()
+
+	/**
+	 * Unserializing instances of this class is forbidden.
+	 * @access public
+	 * @since 1.0.0
+	 */
+	public function __wakeup () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), '1.0.0' );
+	} // End __wakeup()
+
+	/**
+	 * Installation. Runs on activation.
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public function install () {
+		$this->_log_version_number();
+	} // End install()
+
+	/**
+	 * Log the plugin version number.
+	 * @access  private
+	 * @since   1.0.0
+	 */
+	private function _log_version_number () {
+		// Log the version number.
+		update_option( $this->token . '-version', $this->version );
+	} // End _log_version_number()
+} // End Class
